7aae203c5e9bfd6c57e95580de10e39f7bdb2622
ScorpQ <cansinlale01@gmail.com>
2025-07-21 23:18:39 +0300

    feat():

--- src/main/java/group/artifact/security/JwtTokenProvider.java
+++ src/main/java/group/artifact/security/JwtTokenProvider.java
@@ -1,7 +1,13 @@
 package group.artifact.security;
 
+import java.net.Authenticator;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+
+
 
 @Component
 public class JwtTokenProvider {
@@ -11,5 +17,15 @@ public class JwtTokenProvider {
     
     @Value("${questapp.app.expires-in}")
     private Long EXPIRES_IN;
+
+    public String generateJwtToken(Authentication auth) {
+        JwtUserDetails userDetails = (JwtUserDetails) auth.getPrincipal();
+        Date expireDate = new Date(new Date().getTime() + EXPIRES_IN);
+        return JWTVerifier.
+    }
+
+    public Long getUserIdFromJwt(String token) [
+        Claims 
+    ]
     
 }
