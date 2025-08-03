6cff4ce238bef1dfed611bebf65efc96e9145371
ScorpQ <cansinlale01@gmail.com>
2025-08-03 14:21:15 +0300

    Feat(): custom query with JPA's sort feature (!important)

--- src/main/java/group/artifact/repository/PostRepository.java
+++ src/main/java/group/artifact/repository/PostRepository.java
@@ -2,7 +2,9 @@ package group.artifact.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import group.artifact.entities.Post;
@@ -12,4 +14,10 @@ public interface PostRepository extends JpaRepository<Post, String> {
 
     List<Post> findByUserId(Long id);
 
+    @Query("SELECT text, title, likeCount FROM Post P WHERE P.user.id = ?1")
+    List<Post> findTopByUserId(Long id);
+
+    @Query("SELECT text, title, likeCount FROM Post P WHERE P.user.id = ?1")
+    List<Post> findTopByUserId(Long id, Sort sort);
+    
 }
